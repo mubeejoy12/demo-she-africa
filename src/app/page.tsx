075e86c992d31de8aef9950491113f9612cfd917
mubarak <abubakrobasanjo@gmail.com>
2025-04-10 00:50:23 -0700

    updated the page with necessary import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+// import ContentSection from "./components/ContentSection";
+// import { Hero } from "./components/Hero";
+// import ImpactSection from "./components/ImpactSection";
+
 import { HeroText } from "./components/community/HeroText";
 
 export default function Home() {
   return (
     <main>
       <HeroText />
+      {/* <Hero /> */}
+      {/* <ContentSection /> */}
+      {/* <ImpactSection/> */}
     </main>
   );
 }
