2d12fdab183208f0461b63bb3baf2e2c1eb00acf
mubarak <abubakrobasanjo@gmail.com>
2025-04-10 00:49:08 -0700

    imported herotext to the main page to display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import ContentSection from "./components/ContentSection";
-import { Hero } from "./components/Hero";
-import ImpactSection from "./components/ImpactSection";
+import { HeroText } from "./components/community/HeroText";
 
 export default function Home() {
   return (
