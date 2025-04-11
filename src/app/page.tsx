922e34535eaf86cb392edf72b8ae937619aa371a
mubarak <abubakrobasanjo@gmail.com>
2025-04-11 01:08:32 -0700

    uncomment unwanted component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
-// import ContentSection from "./components/ContentSection";
-// import { Hero } from "./components/Hero";
-// import ImpactSection from "./components/ImpactSection";
+
 
 import { HeroText } from "./components/community/HeroText";
+
+
 
 export default function Home() {
   return (
     <main>
       <HeroText />
-      {/* <Hero /> */}
-      {/* <ContentSection /> */}
-      {/* <ImpactSection/> */}
+   
     </main>
   );
 }
